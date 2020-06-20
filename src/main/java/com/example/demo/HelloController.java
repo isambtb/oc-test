244ef6a0b9bd3d94cc803a33ffa5ad03ff6fddcf
isambtb <isambtb@gmail.com>
2020-06-20 20:41:14 +0200

    test

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -2,16 +2,15 @@ package com.example.demo;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("hello")
+@RequestMapping("/hello")
 public class HelloController {
 
-  @GetMapping()
-  public String Sayhello(@PathVariable("name") String name){
-    return "Hello" +  name;
+  @GetMapping
+  public String Sayhello() {
+    return "Hello world!";
   }
 }
